e33df118ce8a28fe10c7802f3cd2bcbc427ba4f0
tanisha-garg <tanishagarg804@gmail.com>
2020-07-07 08:29:12 +0530

    Links updated

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#jumbo-carousel").carousel({ interval: 3000 });
+  });
+  $("#login-btn").click(function(){
+    $('#loginModal').modal('show');
+  });
+  $(".link-div").click(function(){
+    $('#loginModal').modal('show');
+  });
